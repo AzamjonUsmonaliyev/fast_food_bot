845ab4091b95ced5dd1de9fc630746430d125122
A'zamjon Usmonaliyev <azamjonusmon10@gmail.com>
2025-09-03 15:11:25 +0500

    add handler

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from environs import Env
 import asyncio
 import logging
 
+from handler import admin_router,user_router
+
 dp = Dispatcher()
 
 env  = Env()
@@ -12,12 +14,14 @@ env.read_env()
 async def main():
     TOKEN = env.str("TOKEN")
     bot = Bot(TOKEN)
+    dp.include_router(admin_router)
+    dp.include_router(user_router)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    
+
     try:
         asyncio.run(main())
     except Exception as err:
